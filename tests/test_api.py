1b68129a9e764b01a3c0b2dc360269572f77bbde
Ghaith Lashrf <ziyadlas2004@yahoo.com>
2025-12-26 17:14:48 +0000

    Added API test for pytest

---
+++ tests/test_api.py
@@ -0,0 +1,9 @@
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app=app)
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == { "Health": "OK" }
